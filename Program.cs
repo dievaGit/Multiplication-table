45899180d0d2bca85fc372e5d4cc266215cdedc5
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:14:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablaMultiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicacion = 0, numero = 0;
+            
+
+            Console.Write("Escriba un numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for(int cont = 1; cont <= 10; cont ++)
+            {
+                multiplicacion = numero * cont;
+                Console.WriteLine("{0} * {1} = {2}", numero, cont, multiplicacion);
+            }
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
